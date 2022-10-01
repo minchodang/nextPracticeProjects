1ac3ff10c6edffb394976f20e9f98ae34ccec918
minchodang <minsu910725@gmail.com>
2022-10-01 16:31:54 +0900

    Feat: 초기셋팅

---
+++ pages/events/[eventId].tsx
@@ -0,0 +1,12 @@
+import {NextPage} from "next";
+
+
+const EventDetail:NextPage=() => {
+    return (
+        <div>
+            <h1>Event Detail</h1>
+        </div>
+    );
+};
+
+export default  EventDetail;
---
+++ pages/events/index.tsx
@@ -0,0 +1,15 @@
+import {NextPage} from "next";
+
+
+const Events:NextPage = ()=>{
+    return(
+        <div>
+            <h1>All Events</h1>
+        </div>
+    )
+}
+
+
+export default  Events;
+
+

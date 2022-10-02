5cd5a837472f874980bcfd8c80e6f4e0b8f75fea
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-10-03 06:42:06 +0900

    Develop (#4)
    * Feat: 초기 데이터 및 eslint,prettier 설정 추가 (#1)
    
    * Feat: components 추가 (#3)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from 'next';
+import { getFeaturedEvents } from '../dummy-data';
+import { EventList } from '@components/events/EventList';
+
 const Home: NextPage = () => {
+    const featuredEvents = getFeaturedEvents();
     return (
         <div>
-            <h1>The Home Page</h1>
+            <EventList items={featuredEvents} />
         </div>
     );
 };

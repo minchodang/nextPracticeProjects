f74a96516475b6c9facd053fd6fb28252822f08b
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-10-02 17:48:42 +0900

    Feat: 초기 데이터 및 eslint,prettier 설정 추가 (#1)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import type { NextPage } from 'next'
-
-
+import type { NextPage } from 'next';
 const Home: NextPage = () => {
-  return (
-    <div>
-      <h1>The Home Page</h1>
-    </div>
-  )
-}
+    return (
+        <div>
+            <h1>The Home Page</h1>
+        </div>
+    );
+};
 
-export default Home
+export default Home;

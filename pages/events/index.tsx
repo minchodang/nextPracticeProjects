dcb12b6b85d3a54cc99d98f3c1c2bb4d1d962034
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-10-09 16:28:16 +0900

    Develop (#10)
    * Feat: 초기 데이터 및 eslint,prettier 설정 추가 (#1)
    
    * Feat: components 추가 (#3)
    
    * Feat: 이벤트 아이템, 리스트 ui 및 아이콘 추가 (#5)
    
    * Feat: header 및 레이아웃 추가 (#7)
    
    * Feat: eventSearch, document 추가 및 네비게이션 서치 추가 (#9)

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,15 +1,24 @@
-import {NextPage} from "next";
+import { NextPage } from 'next';
+import { getAllEvents } from '../../dummy-data';
+import { EventList } from '@components/events/EventList';
+import { EventSearch } from '@components/events/EventSearch';
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
+const Events: NextPage = () => {
+    const events = getAllEvents();
+    const router = useRouter();
+    const findEventsHandler = (year: string, month: string) => {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    };
 
-const Events:NextPage = ()=>{
-    return(
-        <div>
-            <h1>All Events</h1>
-        </div>
-    )
-}
+    return (
+        <Fragment>
+            <EventSearch onSearch={findEventsHandler} />
+            <EventList items={events} />
+        </Fragment>
+    );
+};
 
-
-export default  Events;
-
-
+export default Events;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from 'next';
 import { getFeaturedEvents } from '../dummy-data';
 import { EventList } from '@components/events/EventList';
+import styled from 'styled-components';
+
+const Container = styled.div``;
 
 const Home: NextPage = () => {
     const featuredEvents = getFeaturedEvents();
     return (
-        <div>
+        <Container>
             <EventList items={featuredEvents} />
-        </div>
+        </Container>
     );
 };
 

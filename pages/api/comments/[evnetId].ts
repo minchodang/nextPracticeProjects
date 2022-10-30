9f861ddd4c83cb87ca6227c6b87b4bf379230fe4
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-10-30 09:38:37 +0900

    Develop (#23)
    * Feat: 초기 데이터 및 eslint,prettier 설정 추가 (#1)
    
    * Feat: components 추가 (#3)
    
    * Feat: 이벤트 아이템, 리스트 ui 및 아이콘 추가 (#5)
    
    * Feat: header 및 레이아웃 추가 (#7)
    
    * Feat: eventSearch, document 추가 및 네비게이션 서치 추가 (#9)
    
    * Feat: slug 페이지 라우팅 분기 처리 로직 작업 추가, ResultsTitle, ErrorAlert 추가 (#11)
    
    * Feat: dataFetching을 위한 정적생성 및 성능 최적화 옵션 추가 (#13)
    
    * Feat: ssr, allEvent 페이지 추가 (#15)
    
    * Feat: clientSideRenering 추가 (#17)
    
    * Feat: api Route 추가 및 코멘트 작업 추가 (#19)
    
    * Feat: newsLetterApi (#20)
    
    * Feat: replyApi 추가 (#22)

--- pages/api/comments/[evnetId].ts
+++ pages/api/comments/[evnetId].ts
@@ -1,11 +1,37 @@
-import { buildCommentsPath, extractComments } from '@pages/api/comments/index';
+import { buildCommentsPath, Data, extractComments } from '@pages/api/comments/index';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-const handler = (req: { query: { commentId: any } }, res: any) => {
-    const commentId = req.query.commentId;
-    const filePath = buildCommentsPath();
-    const commentData = extractComments(filePath);
-    // const selectedComment = commentData.find((comment: { id: any }) => comment.id === commentId);
-    res.status(200).json({ comment: commentData });
+const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const eventId = req.query.eventId;
+    if (req.method === 'POST') {
+        const { email, name, text } = req.body;
+        const filePath = buildCommentsPath();
+        const data = extractComments(filePath);
+
+        if (!email.includes('@') || !name || name.trim() === '' || !text || text.trim() === '') {
+            res.status(422).json({
+                message: 'Invalid input',
+            });
+            return;
+        }
+        const newComment = {
+            id: new Date().toISOString(),
+            email: email,
+            name: name,
+            comment: text,
+        };
+        console.log(newComment);
+        res.status(201).json({ message: 'Added comment', comments: newComment });
+    }
+    if (req.method === 'GET') {
+        const dummyList = [
+            { id: 'c1', name: 'Max', text: 'A first comment!' },
+            { id: 'c2', name: 'Manuel', text: 'A second comment!' },
+        ];
+        res.status(200).json({
+            comments: dummyList,
+        });
+    }
 };
 
 export default handler;

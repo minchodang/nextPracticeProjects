36bfca6bef4451183b62cb879d25aecc7f42ddbd
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-11-19 14:56:17 +0900

    Feat: Notification 추가 (#36)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Layout } from '@components/layout/layout';
+import Notification from '@components/ui/Notification';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <Layout>
             <Component {...pageProps} />
+            <Notification title={'test'} message={'this is a test.'} status={'success'} />
         </Layout>
     );
 }

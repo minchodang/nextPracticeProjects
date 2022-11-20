9c93de360c6e9c7a1d85ab9e2c05ed6eecff2c7a
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-11-20 10:49:33 +0900

    Feature/react context2 (#38)
    * Feat: Notification 추가
    
    * Feat: context 상태 추가

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,15 +1,28 @@
-import { ReactNode } from 'react';
+import { ReactNode, useContext } from 'react';
 import { MainHeader } from '@components/layout/main-header';
+import NotificationContext from '../../store/notification-context';
+import Notification from '@components/ui/Notification';
 
 interface LayoutProps {
     children: ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps) => {
+    const notificationCtx = useContext(NotificationContext);
+
+    const activeNotification = notificationCtx.notification;
+
     return (
         <>
             <MainHeader />
             <main>{children}</main>
+            {activeNotification && (
+                <Notification
+                    title={activeNotification.title}
+                    message={activeNotification.message}
+                    status={activeNotification.status}
+                />
+            )}
         </>
     );
 };
